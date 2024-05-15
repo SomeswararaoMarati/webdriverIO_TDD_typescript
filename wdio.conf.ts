93623b0f0f117fded379f3562750cc423337a21a
SomeswararaoMarati <31479064+SomeswararaoMarati@users.noreply.github.com>
2024-05-15 08:40:29 +0530

    Update wdio.conf.ts

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -58,9 +58,13 @@ export const config: Options.Testrunner = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
     //
-    capabilities: [{
-        browserName: 'chrome'
-    }],
+    capabilities: {
+          browserName: 'firefox',
+          'moz:firefoxOptions': {
+            args: ['-headless']  // Add this argument for headless mode
+          }
+        }
+    
 
     //
     // ===================
