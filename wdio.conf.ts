1dd623d0a30d06840f90679e94272b98a4fc5be0
SomeswararaoMarati <31479064+SomeswararaoMarati@users.noreply.github.com>
2024-05-15 09:22:45 +0530

    Update wdio.conf.ts

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -60,7 +60,7 @@ export const config: Options.Testrunner = {
     //
   capabilities: {
       browserName: 'chrome',
-      alwaysMatch: { // Optional: Additional properties that should always apply
+      alwaysMatch: {
         acceptInsecureCerts: false  // Example
       }
     },
@@ -72,7 +72,7 @@ export const config: Options.Testrunner = {
     // Define all options that are relevant for the WebdriverIO instance here
     //
     // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'silent',
+    logLevel: 'info',
     //
     // Set specific log levels per logger
     // loggers:
