ec7a217a33fa5d461bf22862c9ad1776ec5ecbe7
someswararao.m <someswararao.m@gmail.com>
2024-05-18 12:20:08 +0530

    Changed the loglvel

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -69,7 +69,7 @@ export const config: Options.Testrunner = {
     // Define all options that are relevant for the WebdriverIO instance here
     //
     // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'silent',
+    logLevel: 'info',
     //
     // Set specific log levels per logger
     // loggers:
